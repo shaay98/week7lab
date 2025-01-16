54238f9d01090d6025923eddce7fefeb75fd0cce
shaay98 <nrichardson567@icloud.com>
2025-01-16 03:07:05 +0000

    week7

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-"use strict"
+"use strict";
 
 let weather = [
-    { day: "Sunday", temperture: 60, rainfall: 0.2},
-    { day: "Monday", temperture: 55, rainfall: 0},
-    { day: "Tuesday", temperture: 63, rainfall: 0.3},
-    { day: "Wednesday", temperture: 57, rainfall: 0.25},
+  { day: "Sunday", temperture: 60, rainfall: 0.2 },
+  { day: "Monday", temperture: 55, rainfall: 0 },
+  { day: "Tuesday", temperture: 63, rainfall: 0.3 },
+  { day: "Wednesday", temperture: 57, rainfall: 0.25 },
 ];
 
-let tb = document.getElementById("weather")
-weather.forEach(weatherDay => {
-   let tr= `<tr>
+let tb = document.getElementById("weather");
+weather.forEach((weatherDay) => {
+  let tr = `<tr> 
             <td>${weatherDay.day}</td>
             <td>${weatherDay.temperture}</td>
             <td>${weatherDay.rainfall}</td>
-          </tr>`
-        tb.innerHTML += tr;
-}); 
+          </tr>`;
+  tb.innerHTML += tr;
+});
 
+let tempTotal = 0;
+let days = weatherDay.length;
 
+weatherDay.forEach((day) => {
+  tempTotal += day.temperature;
+});
+let tempTotal = tempTotal / days;
+document.getElementById("temp").addEventListener;
+
+let maxRain = weatherDay.reduce((day) => {
+  return day.rainfall > max.Rain;
+});
+document.getElementById("maxRain").addEventListener;
