9ccc33f74ae11ec9cf0688c94ac00c8d3271083c
shaay98 <nrichardson567@icloud.com>
2025-01-16 01:53:33 +0000

    week7

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 "use strict"
 
+let weather = [
+    { day: "Sunday", temperture: 60, rainfall: 0.2},
+    { day: "Monday", temperture: 55, rainfall: 0},
+    { day: "Tuesday", temperture: 63, rainfall: 0.3},
+    { day: "Wednesday", temperture: 57, rainfall: 0.25},
+];
+
 let weatherArray = [{day, temperture, rainfall}]
 
 array.forEach(weatherArray => {
     
 });
+
